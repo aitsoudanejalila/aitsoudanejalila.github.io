c770681e7fda2ea7d83a5b8768f6dd808936e9fd
Mouad Cherkaoui <cherkaoui.mouad@live.fr>
2019-11-03 17:15:00 +0100

    executing the rendering on document ready

--- assets/js/templating.js
+++ assets/js/templating.js
@@ -1,55 +1,57 @@
-var items =
-    [
-        {
-            "id": 1,
-            "text": "Acceuil",
-            "link": "/"
-        },
-        {
-            "id": 2,
-            "text": "Publications et Communications",
-            "hasSubItems": true,
-            "items": [
-                { "text": "Publications", "link": "/communications" },
-                { "text": "Communications", "link": "/publications" }                
-            ]
-        },
-        {
-            "id": 3,
-            "text": "Activites et Services",
-            "hasSubItems": true,
-            "items": [
-                { "text": "Activites", "link": "/activites" },
-                { "text": "Services", "link": "/services" }                
-            ]
-        },
-        {
-            "id": 4,
-            "text": "Enseignement",
-            "link": "/enseignement"
-        },
-        {
-            "id": 5,
-            "text": "Evenements",
-            "link": "/evenements"
-        },
-        {
-            "id": 6,
-            "text": "Biographie",
-            "link": "/biographie"
-        },
-        {
-            "id": 7,
-            "text": "Formation",
-            "hasSubItems": true,
-            "items": [
-                { "text": "License", "link": "/license" },
-                { "text": "Matser", "link": "/master" },
-                { "text": "Doctorat", "link": "/doctorat" },                
-            ]
-        }
-    ];
-$.Mustache.load('./templates/items-template.html')
-    .done(function () {
-        $('.items').mustache('item-tmpl', items);
-});
+$(document).ready(function() {
+    var items =
+        [
+            {
+                "id": 1,
+                "text": "Acceuil",
+                "link": "/"
+            },
+            {
+                "id": 2,
+                "text": "Publications et Communications",
+                "hasSubItems": true,
+                "items": [
+                    { "text": "Publications", "link": "/communications" },
+                    { "text": "Communications", "link": "/publications" }                
+                ]
+            },
+            {
+                "id": 3,
+                "text": "Activites et Services",
+                "hasSubItems": true,
+                "items": [
+                    { "text": "Activites", "link": "/activites" },
+                    { "text": "Services", "link": "/services" }                
+                ]
+            },
+            {
+                "id": 4,
+                "text": "Enseignement",
+                "link": "/enseignement"
+            },
+            {
+                "id": 5,
+                "text": "Evenements",
+                "link": "/evenements"
+            },
+            {
+                "id": 6,
+                "text": "Biographie",
+                "link": "/biographie"
+            },
+            {
+                "id": 7,
+                "text": "Formation",
+                "hasSubItems": true,
+                "items": [
+                    { "text": "License", "link": "/license" },
+                    { "text": "Matser", "link": "/master" },
+                    { "text": "Doctorat", "link": "/doctorat" },                
+                ]
+            }
+        ];
+    $.Mustache.load('./templates/items-template.html')
+        .done(function () {
+            $('.items').mustache('item-tmpl', items);
+    });
+})
