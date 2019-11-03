5c50f4fdc05a8ad84322bcc54611a9ce3e2cdecf
Mouad Cherkaoui <cherkaoui.mouad@live.fr>
2019-11-03 17:11:31 +0100

    testing sidebar template

--- assets/js/templating.js
+++ assets/js/templating.js
@@ -2,23 +2,51 @@ var items =
     [
         {
             "id": 1,
-            "text": "Sarah",
-            "link": 35
+            "text": "Acceuil",
+            "link": "/"
         },
         {
             "id": 2,
-            "text": "Tricia",
-            "link": 38
+            "text": "Publications et Communications",
+            "hasSubItems": true,
+            "items": [
+                { "text": "Publications", "link": "/communications" },
+                { "text": "Communications", "link": "/publications" }                
+            ]
         },
         {
             "id": 3,
-            "text": "Joanna",
-            "link": 29
+            "text": "Activites et Services",
+            "hasSubItems": true,
+            "items": [
+                { "text": "Activites", "link": "/activites" },
+                { "text": "Services", "link": "/services" }                
+            ]
         },
         {
             "id": 4,
-            "text": "Libby",
-            "link": 37
+            "text": "Enseignement",
+            "link": "/enseignement"
+        },
+        {
+            "id": 5,
+            "text": "Evenements",
+            "link": "/evenements"
+        },
+        {
+            "id": 6,
+            "text": "Biographie",
+            "link": "/biographie"
+        },
+        {
+            "id": 7,
+            "text": "Formation",
+            "hasSubItems": true,
+            "items": [
+                { "text": "License", "link": "/license" },
+                { "text": "Matser", "link": "/master" },
+                { "text": "Doctorat", "link": "/doctorat" },                
+            ]
         }
     ];
 $.Mustache.load('./templates/items-template.html')
