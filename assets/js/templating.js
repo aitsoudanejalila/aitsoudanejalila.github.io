37367f8d4cc5a9791dc16a7ef2114b20b83f10fa
Mouad Cherkaoui <cherkaoui.mouad@live.fr>
2019-11-07 23:42:59 +0100

    making header in a template file

--- assets/js/templating.js
+++ assets/js/templating.js
@@ -352,9 +352,9 @@ window.setupSidebar = function() {
 window.loadHeader = function(){
 	$.Mustache.load('/templates/header-template.html')
 	.done(function () {
-		var activeItem = window.navItems.find(i => i.active === 'active');
-		if(activeItem.items != 'undefined') {
-			title = activeItem.items.find(i => i.active === 'active')._text;
+		var activeItem = window.navItems.find(i => i.active === "active");
+		if(activeItem.items !== undefined) {
+			title = activeItem.items.find(i => i.active === "active")._text;
 		} else title = activeItem.text;
 		$("#header").mustache('header-tmpl', { title: title });
 	});
